5c5e87dadd0e9fcadc35df7814757ebce93c1894
POAPCA02LC0530 <SSDD@POAPCA02LC0530.duoc.acad>
2025-08-28 21:02:47 -0400

    Segundo commit

---
+++ funciones.js
@@ -0,0 +1,53 @@
+document
+  .getElementById("formularioContacto")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const nombre = document.getElementById("nombre");
+    const email = document.getElementById("email");
+    const numero = document.getElementById("numero");
+    const pwd = document.getElementById("pwd");
+    const comentarios = document.getElementById("comentarios");
+
+    let isValid = true;
+
+    if (!nombre.value.trim()) {
+      nombre.classList.add("is-invalid");
+      isValid = false;
+    } else {
+      nombre.classList.remove("is-invalid");
+    }
+
+    if (!email.value.trim()) {
+      email.classList.add("is-invalid");
+      isValid = false;
+    } else {
+      email.classList.remove("is-invalid");
+    }
+
+    if (!numero.value.trim()) {
+      numero.classList.add("is-invalid");
+      isValid = false;
+    } else {
+      numero.classList.remove("is-invalid");
+    }
+
+    if (!pwd.value.trim()) {
+      pwd.classList.add("is-invalid");
+      isValid = false;
+    } else {
+      pwd.classList.remove("is-invalid");
+    }
+
+    if (!comentarios.value.trim()) {
+      comentarios.classList.add("is-invalid");
+      isValid = false;
+    } else {
+      comentarios.classList.remove("is-invalid");
+    }
+    if (isValid) {
+      alert("Formulario enviado correctamente");
+    } else {
+      alert("Por favor, corrige los campos marcados");
+    }
+    return false;
+  });
